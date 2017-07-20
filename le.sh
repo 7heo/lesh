dbe5cee6afc604cdbcf0e369d0addd5fc12ed375
7heo <7heo@mail.com>
2017-07-20 20:40:21 +0200

    FIX le.sh: POSIX and sh

--- le.sh
+++ le.sh
@@ -1,7 +1,15 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 # shell script hardening
-set -euf -o pipefail
+set -e
+set -u
+set -f
+
+# TODO: Harden the script when commands fail before a pipe
+
+# TODO: Allow generating two certificates for the purpose of HPKP
+
+# TODO: Check dependencies before executing
 
 #
 # Lets Encrypt Certificate Generator
@@ -12,36 +20,36 @@ set -euf -o pipefail
 # negotiate with the Lets Encrypt ACME server to save a signed certificate
 # chain.
 #
-# dependency: bash (/dev/fd), openssl, curl
+# dependency: POSIX sh (POSIX files and utilities - /dev/null, xargs, sed, mkdir, cut, tr, ...), /dev/stdin, /dev/random, openssl, curl
 
 ################ options start#################
 
 # The primary domain name followed by any alternative names we are requesting a
 # certificate for. Use a space separated list. Each domain name will be tested
-# by the ACME server. 
+# by the ACME server.
 #DOMAINS="example.org www.example.org mail.example.org"
-DOMAINS="example.org www.example.org"
+DOMAINS="${DOMAINS:-"example.org www.example.org"}"
 
 # The directory the script is run from and where all certificates will be
 # stored under. This directory should be secure and not under the web root.
-BASEDIR="/tools/lets_encrypt"
+BASEDIR="${BASEDIR:-"/tools/lets_encrypt"}"
 
 # The full path to the web directory our script will write the temporary
 # negotiation file. The Lets Encrypt service will then connect to our web
 # server to collect this temporary file verifying we own the domain. Our web
 # server can serve the file through http or https and 301 redirection are
 # allowed.
-WEBDIR="/var/www/.well-known/acme-challenge"
+WEBDIR="${WEBDIR:-"/var/www/.well-known/acme-challenge"}"
 
 # SSL Certificates type. RSA at 2048 bit should be used for wider compatability
 # including older clients. ECDSA prime 256 bits is prefered for its smaller key
 # size, faster server side processing and better security model. Options: "rsa"
 # or "ecdsa"
-CERTTYPE="rsa"
+CERTTYPE="${CERTTYPE:-"rsa"}"
 #CERTTYPE="ecdsa"
 
 # The Lets Encrypt certificate authority URL
-CA="https://acme-staging.api.letsencrypt.org" # testing server, high rate limits. "Fake LE Intermediate X1"
+CA="${CA:-"https://acme-staging.api.letsencrypt.org"}" # testing server, high rate limits. "Fake LE Intermediate X1"
 #CA="https://acme-v01.api.letsencrypt.org"      # official server, rate limited to 5 certs per 7 days
 
 ################ options end ##################
@@ -56,33 +64,27 @@ ROOTCERT="lets-encrypt-x3-cross-signed.pem.txt"
 # check the path to the openssl configuration file
 OPENSSL_CNF="$(openssl version -d | cut -d'"' -f2)/openssl.cnf"
 
+# TODO: Add checks for options
+
+rand() {
+  # NOTE: /dev/random and /dev/urandom aren't POSIX.
+  od -An -N${1:-7} -tuI /dev/urandom | tr -d ' '
+}
 
 urlbase64() {
   # urlbase64: base64 encoded string with '+' replaced with '-' and '/' replaced with '_'
-  openssl base64 -e | tr -d '\n\r' | sed 's/=*$//g' | tr '+/' '-_'
+  openssl base64 -e | tr -d '\n\r' | sed 's/=*$//' | tr '+/' '-_'
 }
 
 hex2bin() {
-  # Store hex string from stdin
-  tmphex="$(cat)"
+  # Store hex string from stdin and strip spaces
+  hex="$(cat | tr -d ' ')"
 
-  # Remove spaces
-  hex=''
-  for ((i=0; i<${#tmphex}; i+=1)); do
-    test "${tmphex:$i:1}" == " " || hex="${hex}${tmphex:$i:1}"
-  done
+  # Add leading zero if necessary
+  [ $((${#hex} % 2)) -eq 0 ] || hex="0${hex}" # N.B: This syntax is POSIX - http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
 
-  # Add leading zero
-  test $((${#hex} & 1)) == 0 || hex="0${hex}"
-
-  # Convert to escaped string
-  escapedhex=''
-  for ((i=0; i<${#hex}; i+=2)); do
-    escapedhex=${escapedhex}\\x${hex:$i:2}
-  done
-
-  # Convert to binary data
-  printf -- "${escapedhex}"
+  # Convert to escaped string and print binary out
+  printf "%s" "${hex}" | sed 's/../\\x&/g' | xargs -0 printf --
 }
 
 _request() {
@@ -97,11 +99,11 @@ _request() {
          statuscode="$(curl -s -w "%{http_code}" -o "${tempcont}" "${2}" -d "${3}")" ;;
   esac
 
-  if [[ ! "${statuscode:0:1}" = "2" ]]; then
+  if [ "$(printf "%s" "${statuscode}" | cut -b1)" != "2" ]; then
     printf '%s\n' "   ERROR: sending ${1}-request to ${2} (Status ${statuscode})" >&2
     printf '%s\n' >&2
     printf '%s\n' "Details:" >&2
-    printf '%s\n' "$(<"${tempcont}"))" >&2
+    printf '%s\n' "$(cat "${tempcont}"))" >&2
     rm -f "${tempcont}"
     exit 1
   fi
@@ -114,7 +116,7 @@ thumb_print() {
   # Collect the public components from the new private key and calculate the
   # thumbprint which the ACME server will challenge
   pubExponent64="$(printf "%06x" "$(openssl rsa -in "${BASEDIR}/private_account_key.pem" -noout -text | grep publicExponent | head -1 | cut -d' ' -f2)" | hex2bin | urlbase64)"
-  pubMod64="$(printf '%s' "$(openssl rsa -in "${BASEDIR}/private_account_key.pem" -noout -modulus | cut -d'=' -f2)" | hex2bin | urlbase64)"
+  pubMod64="$(printf '%s' "$(openssl rsa -in "${BASEDIR}/private_account_key.pem" -noout -modulus | cut -d= -f2)" | hex2bin | urlbase64)"
   thumbprint="$(printf '%s' "$(printf '%s' '{"e":"'"${pubExponent64}"'","kty":"RSA","n":"'"${pubMod64}"'"}' | shasum -a 256 | awk '{print $1}')" | hex2bin | urlbase64)"
 }
 
@@ -146,9 +148,9 @@ sign_domain() {
   altnames="${*}"
 
   # create a directory to keep the domain's certificates in
-  if [[ ! -e "${BASEDIR}/${domain}" ]]; then
+  if [ ! -e "${BASEDIR}/${domain}" ]; then
     printf " + Make directory ${BASEDIR}/${domain}\n"
-    mkdir -p "${BASEDIR}/${domain}" 
+    mkdir -p "${BASEDIR}/${domain}"
   fi
 
   # Create a new private key for the domain. To add a bit of entropy to the
@@ -158,27 +160,28 @@ sign_domain() {
   # seed virtual machine (VM) entropy.
     printf " + Seed entropy by generating random keys:"
     START=1
-    END=$(( RANDOM % (10 - 5 + 1 ) + 5 ))
-    for (( i=$START; i<=$END; i++ ))
-      do
-       printf " ${i}" 
-       case "${CERTTYPE}" in
-        "rsa" )
-          openssl genrsa -out "${BASEDIR}/${domain}/${domain}-privatekey.pem" 2048 2> /dev/null > /dev/null ;;
-        "ecdsa" )
-          openssl ecparam -genkey -name prime256v1 -out "${BASEDIR}/${domain}/${domain}-privatekey.pem" 2> /dev/null > /dev/null ;;
-       esac
-      done
+    END=$(( $(rand) % (10 - 5 + 1 ) + 5 ))
+    i=$START
+    while [ $i -le $END ]; do
+     printf " $i"
+     case "$CERTTYPE" in
+      "rsa" )
+        openssl genrsa -out "${BASEDIR}/${domain}/${domain}-privatekey.pem" 2048 2> /dev/null > /dev/null ;;
+      "ecdsa" )
+        openssl ecparam -genkey -name prime256v1 -out "${BASEDIR}/${domain}/${domain}-privatekey.pem" 2> /dev/null > /dev/null ;;
+     esac
+     i=$(($i + 1))
+    done
     printf "\n + Private Key created\n"
 
   # Generate a signing request
   SAN=""
   for altname in ${altnames}; do
-    SAN+="DNS:${altname}, "
+    SAN="${SAN}DNS:${altname}, "
   done
-  SAN="${SAN%%, }"
+  SAN="${SAN%%, }" # N.B: This syntax is POSIX - http://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html
   printf " + Generate signing request\n"
-  openssl req -new -sha256 -key "${BASEDIR}/${domain}/${domain}-privatekey.pem" -out "${BASEDIR}/${domain}/${domain}-certsignrequest.csr" -subj "/CN=${domain}/" -reqexts SAN -config <(cat "${OPENSSL_CNF}" <(printf "[SAN]\nsubjectAltName=%s" "${SAN}")) > /dev/null
+  printf "[SAN]\nsubjectAltName=%s\n" "${SAN}" | cat "${OPENSSL_CNF}" - | openssl req -new -sha256 -key "${BASEDIR}/${domain}/${domain}-privatekey.pem" -out "${BASEDIR}/${domain}/${domain}-certsignrequest.csr" -subj "/CN=${domain}/" -reqexts SAN -config /dev/stdin > /dev/null
 
   # Request and respond to challenges
   for altname in ${altnames}; do
@@ -186,12 +189,12 @@ sign_domain() {
     printf " + Request challenge for ${altname}\n"
     response="$(signed_request "${CA}/acme/new-authz" '{"resource": "new-authz", "identifier": {"type": "dns", "value": "'"${altname}"'"}}')"
 
-    challenges="$(printf '%s\n' "${response}" | grep -Eo '"challenges":[^\[]*\[[^]]*]')"
-    challenge="$(printf "%s" "${challenges//\{/$'\n'{}}" | grep 'http-01')"
-    challenge_token="$(printf '%s' "${challenge}" | grep -Eo '"token":\s*"[^"]*"' | cut -d'"' -f4 | sed 's/[^A-Za-z0-9_\-]/_/g')"
-    challenge_uri="$(printf '%s' "${challenge}" | grep -Eo '"uri":\s*"[^"]*"' | cut -d'"' -f4)"
+    challenges="$(printf '%s\n' "${response}" | tr -d '\n' | grep -Eo '"challenges":[^\[]*\[[^]]*]')"
+    challenge="$(printf "%s" "${challenges}" | tr '{}' '\n' | grep 'http-01')"
+    challenge_token="$(printf '%s' "${challenge}" | grep -Eo '"token":[[:space:]]*"[^"]*"' | cut -d'"' -f4 | sed 's/[^A-Za-z0-9_\-]/_/g')"
+    challenge_uri="$(printf '%s' "${challenge}" | grep -Eo '"uri":[[:space:]]*"[^"]*"' | cut -d'"' -f4)"
 
-    if [[ -z "${challenge_token}" ]] || [[ -z "${challenge_uri}" ]]; then
+    if [ -z "${challenge_token}" ] || [ -z "${challenge_uri}" ]; then
       printf "    Error: Can't retrieve challenges (${response})\n"
       exit 1
     fi
@@ -199,7 +202,7 @@ sign_domain() {
     # Challenge response consists of the challenge token and the thumbprint of our public certificate
     keyauth="${challenge_token}.${thumbprint}"
 
-    # Store challenge response in the web directory 
+    # Store challenge response in the web directory
     printf '%s' "${keyauth}" > "${WEBDIR}/${challenge_token}"
     chmod a+r "${WEBDIR}/${challenge_token}"
 
@@ -207,19 +210,19 @@ sign_domain() {
     printf " + Respond to challenge for ${altname}\n"
     result="$(signed_request "${challenge_uri}" '{"resource": "challenge", "keyAuthorization": "'"${keyauth}"'"}')"
 
-    status="$(printf '%s\n' "${result}" | grep -Eo '"status":\s*"[^"]*"' | cut -d'"' -f4)"
+    status="$(printf '%s\n' "${result}" | grep -Eo '"status":[[:space:]]*"[^"]*"' | cut -d'"' -f4)"
 
-    # Loop until the status of the request is accepted    
-    while [[ "${status}" = "pending" ]]; do
+    # Loop until the status of the request is accepted
+    while [ "${status}" = "pending" ]; do
       sleep 1
-      status="$(_request get "${challenge_uri}" | grep -Eo '"status":\s*"[^"]*"' | cut -d'"' -f4)"
+      status="$(_request get "${challenge_uri}" | grep -Eo '"status":[[:space:]]*"[^"]*"' | cut -d'"' -f4)"
     done
 
     # Remove the temporary challenge file from the web directory
     rm -f "${WEBDIR}/${challenge_token}"
 
     # Check the status of the ACME server negotiation
-    if [[ "${status}" = "valid" ]]; then
+    if [ "${status}" = "valid" ]; then
       printf " + Challenge accepted\n"
     else
       printf "   Challenge is invalid ! (returned: ${status})\n"
@@ -234,18 +237,18 @@ sign_domain() {
   crt64="$(signed_request "${CA}/acme/new-cert" '{"resource": "new-cert", "csr": "'"${csr64}"'"}' | openssl base64 -e)"
   printf -- '-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----\n' "${crt64}" > "${BASEDIR}/${domain}/${domain}-certchain.pem"
 
-  # add the intermediate lets encrypt public certificate to the chain 
+  # add the intermediate lets encrypt public certificate to the chain
   printf " + Add intermediate certificate to chain\n"
   cat "${BASEDIR}/${ROOTCERT}" >> "${BASEDIR}/${domain}/${domain}-certchain.pem"
 
   printf " + Complete.\n"
-  
+
 }
 
 inspect() {
   domain="${1}"
 
-  rootcerts="/etc/ssl/" 
+  rootcerts="/etc/ssl/"
   # location of FreeBSD's root certificates
   if [ -f /etc/ssl/cert.pem ]; then
      rootcerts="/etc/ssl/cert.pem"
@@ -257,9 +260,9 @@ inspect() {
   case "${CERTTYPE}" in
   "rsa" )
      printf "\nMD5 signatures must be equal\n\n"
-     md5privatekey="$(openssl rsa -noout -modulus -in ${BASEDIR}/${domain}/${domain}-privatekey.pem | openssl md5)" 
+     md5privatekey="$(openssl rsa -noout -modulus -in ${BASEDIR}/${domain}/${domain}-privatekey.pem | openssl md5)"
      md5certsignrequest="$(openssl req -noout -modulus -in ${BASEDIR}/${domain}/${domain}-certsignrequest.csr | openssl md5)"
-     md5certchain="$(openssl x509 -noout -modulus -in ${BASEDIR}/${domain}/${domain}-certchain.pem | openssl md5)" 
+     md5certchain="$(openssl x509 -noout -modulus -in ${BASEDIR}/${domain}/${domain}-certchain.pem | openssl md5)"
      printf "   Private Key   = ${md5privatekey}\n"
      printf "   Cert Sign Req = ${md5certsignrequest}\n"
      printf "   Cert Chain    = ${md5certchain}\n" ;;
@@ -282,8 +285,9 @@ inspect() {
 
   printf "\nHTTP Key Pinning\n   pin-sha256=\"${hpkp}\";\n"
 
+  # TODO: De-activate the last check in case the CA isn't the v01 one.
   # check the issuer field and the full certificate path against the system's root certificate chain
-  printf "\nVerify the authority and certificate chain\n" 
+  printf "\nVerify the authority and certificate chain\n"
   printf "   "; openssl x509 -noout -in ${domain}/${domain}-certchain.pem -issuer
   printf "   "; openssl verify -CApath ${rootcerts} ${ROOTCERT}
   printf "   "; openssl verify -CApath ${rootcerts} -untrusted ${ROOTCERT} ${domain}/${domain}-certchain.pem
@@ -301,13 +305,22 @@ printf "    ------------------------------------\n"
 printf "\nInitialize the environment\n\n"
 
 # Change directory to BASEDIR
-  cd ${BASEDIR}
+  cd "${BASEDIR}"
+  [ "$BASEDIR" != "$PWD" ] && BASEDIR="$PWD"
+
+  for DOMAIN in ${DOMAINS}; do
+    printf " + Checking that http://${DOMAIN}/.well-known/acme-challenge/ is served under directory '${WEBDIR}'\n"
+    proof=$(rand)
+    printf "%s\n" "$proof" > "${WEBDIR}/$proof"
+    curl -SsL "http://${DOMAIN}/.well-known/acme-challenge/$proof" | grep -q '^'"$proof"'$'
+    rm "${WEBDIR}/$proof"
+  done
 
 # Update the Lets Encrypt Authority PEM certificate
   printf " + Update the Lets Encrypt Authority PEM certificate\n"
-  curl -sS -L -o ${BASEDIR}/${ROOTCERT} https://letsencrypt.org/certs/${ROOTCERT}
+  curl -sS -L -o "${BASEDIR}/${ROOTCERT}" "https://letsencrypt.org/certs/${ROOTCERT}"
 
-# Generate a new account key 
+# Generate a new account key
   printf " + Generate new private account key\n"
   openssl genrsa -out "${BASEDIR}/private_account_key.pem" "4096" 2> /dev/null > /dev/null
 
@@ -322,12 +335,7 @@ printf "\nInitialize the environment\n\n"
 
 # Generate certificate for the domain
   printf "\nGenerate certificate for ${DOMAINS}\n\n"
-  sign_domain ${DOMAINS}
+  sign_domain "${DOMAINS}"
 
 # Visually inspect the MD5 hashes
-  inspect ${DOMAINS} 
-
-
-#
-##
-### EOF ###
+  inspect "${DOMAINS}"
